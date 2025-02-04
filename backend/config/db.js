cae4681b81597842a1edb01aa03c11e77daa599d
Konstantinos Mourousidis <kwstasmr7@gmail.com>
2025-02-04 12:19:58 +0200

    Update Set up

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,10 +2,14 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-        mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(process.env.MONGO_URI, {
+            serverSelectionTimeoutMS: 10000, // Timeout after 10 seconds
+        });
         console.log("MongoDB Connected!");
     } catch (error) {
         console.error(`Error: ${error.message}`);
         process.exit(1);
     }
-}
+};
+
+
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,31 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import { connectDB } from "./config/db.js";
-
 dotenv.config();
-const app = express();
-const port = 8080;
 
+import { connectDB } from "./config/db.js";
 connectDB();
 
+// import all the routes
+import profileRoutes from "./components/profile/profile.routes.js";
+// const education = require('./components/education')
+// const projects = require('./components/projects')
+// const experience = require('./components/experience')
+// const certifications = require('./components/certifications')
+// const skills = require('./components/skills')
+// const cvs = require('./components/cvs')
+
+const app = express();
+app.use(express.json()); // allow us to accept JSON data in the req.body
+
+app.use("/api/profile", profileRoutes);
+// app.use("/api/education", education);
+// app.use("/api/projects", projects);
+// app.use("/api/experience", experience);
+// app.use("/api/certifications", certifications);
+// app.use("/api/skills", skills);
+// app.use("/api/cvs", cvs);
+
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
     console.log("Website served on http://localhost:" + port);
 });
