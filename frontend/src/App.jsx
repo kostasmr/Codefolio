1b4df7cf25bca3612d3c15ba2af00d2dd91120e8
Konstantinos Mourousidis <kwstasmr7@gmail.com>
2025-06-28 20:40:10 +0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,13 @@
-import { Box, useColorModeValue } from "@chakra-ui/react";
 import { Routes, Route } from "react-router-dom";
-// import HomePage from "./pages/HomePage";
 import PortfolioPage from "./pages/PortfolioPage";
 
 function App() {
 
   return (
     <>
-      <Box minH={"100vh"} bg={useColorModeValue("gray.200","black")} as="main" flex="1">
         <Routes>
-          {/* <Route path="/" element={<HomePage />} /> */}
           <Route path="/portfolio" element={<PortfolioPage />} />
-          {/* <Route path="/" element={<CreatePage />} /> */}
         </Routes>
-      </Box>
     </>
   )
 }
