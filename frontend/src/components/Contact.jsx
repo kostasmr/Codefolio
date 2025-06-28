8afc6391ded6913c5737ecf232147ab3fcb2d378
Konstantinos Mourousidis <kwstasmr7@gmail.com>
2025-06-28 20:39:48 +0300

    Create Contact.jsx

---
+++ frontend/src/components/Contact.jsx
@@ -0,0 +1,67 @@
+import gradientImg from '../assets/images/letswork-image.png';
+import { Container, Image, Text, Flex} from "@chakra-ui/react";
+
+function Contact () {
+    return (
+        <>
+            <Container maxW="100%" height="100vh" padding={5} bg={"black"} position={'relative'}>
+                <Image
+                    src={gradientImg}
+                    alt="Gradient Image"
+                    objectFit="cover"
+                    width="100%"
+                    height="100%"
+                />
+                <Flex 
+                    position="absolute"
+                    top={0}
+                    left={0}
+                    width="100%"
+                    height="100%"
+                    direction="column"
+                    justify="space-between"
+                    color="white"
+                    px={10}
+                    py={6}
+                    zIndex={1}
+                >
+                    <Flex
+                        justifyContent={"space-between"}
+                        alignItems={"center"}
+                        paddingInline={"40px"}
+                        paddingTop={"20px"}
+                    > 
+                        <Text fontWeight={"bold"}>Thessaloniki, Greece</Text>                
+                        <Text>+30 697 596 0090</Text>
+                    </Flex>
+
+                    <Flex 
+                        justify={"center"}
+                        align={"center"}
+                        h={"full"}                        
+                    >
+                        <Text 
+                            fontSize={"40px"} 
+                            fontWeight={"bold"} 
+                            letterSpacing={"10px"} 
+                            align={"center"} 
+                            >
+                                LETS WORK <br /> TOGETHER
+                            </Text>
+                    </Flex>
+                    <Flex                    
+                        paddingInline={"40px"}
+                        paddingBottom={"20px"}
+                        justifyContent={"space-between"}
+                    >
+                        <Text fontWeight={"bold"}>Send me a message</Text>
+                        <Text marginRight={"70px"}>kmourousidis@gmail.com</Text>
+                        <Text>linkedin github</Text>
+                    </Flex>
+                </Flex>
+            </Container>
+        </>
+    )
+}
+    
+export default Contact
