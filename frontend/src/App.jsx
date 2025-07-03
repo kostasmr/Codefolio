5daadb7635b3fad4319d1cfae66a6114f458d96f
Konstantinos Mourousidis <kwstasmr7@gmail.com>
2025-07-03 14:04:47 +0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
         <Routes>
-          <Route path="/portfolio" element={<PortfolioPage />} />
+          <Route path="/" element={<PortfolioPage />} />
         </Routes>
     </>
   )
