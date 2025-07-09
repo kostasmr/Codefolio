1603270db3df7180f5a409b258a68ab206c8a576
Konstantinos Mourousidis <kwstasmr7@gmail.com>
2025-07-09 22:43:04 +0300

    Update Projects.jsx

--- frontend/src/components/Projects.jsx
+++ frontend/src/components/Projects.jsx
@@ -29,7 +29,7 @@ function Projects () {
 
     return (
         <>
-            <Container maxW="100%" height="auto" bg={"black"} paddingTop={"200px"}>
+            <Container id="projects" maxW="100%" height="auto" bg={"black"} paddingTop={"200px"}>
                 <Flex justify={"space-between"} paddingInline={"10%"}>
                     <Text fontSize={"26px"}>Projects</Text> 
                     <Text>/04</Text>
