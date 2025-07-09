a046a9b068afb16f7ffc58d5b18650c282facc95
Konstantinos Mourousidis <kwstasmr7@gmail.com>
2025-07-09 22:43:13 +0300

    Update PortfolioPage.jsx

--- frontend/src/pages/PortfolioPage.jsx
+++ frontend/src/pages/PortfolioPage.jsx
@@ -6,6 +6,7 @@ import Experience from "../components/Experience";
 import Projects from "../components/Projects";
 import Contact from "../components/Contact";
 import Skills from "../components/Skills";
+import ScrollToTopButton from "../components/ScrollOnTop";
 
 
 
@@ -21,7 +22,7 @@ const PortfolioPage = () => {
       <Projects/>
       <Skills/>
       <Contact/>
-    
+      <ScrollToTopButton/>
     </>
   )
 }
