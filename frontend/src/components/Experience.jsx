84db2e8371e332373012ee7453380aa4fd1889ee
Konstantinos Mourousidis <kwstasmr7@gmail.com>
2025-07-15 15:04:15 +0300

    Update Experience.jsx

--- frontend/src/components/Experience.jsx
+++ frontend/src/components/Experience.jsx
@@ -1,5 +1,6 @@
 import gradientImg from '../assets/images/experience-image.png'
-import { Container, Text, Image, Flex} from "@chakra-ui/react";
+import { Container, Text} from "@chakra-ui/react";
+import JobCard from './JobCard';
 
 function Experience () {
     return (
@@ -7,22 +8,15 @@ function Experience () {
             <Container id='experience' maxW="100%" height="auto" bg={"black"} paddingTop={"200px"}>
                 <Text align={"center"}>/03</Text>
                 <Text align={"center"} marginTop={"80px"} fontSize={"26px"}>Experience</Text> 
-                <Flex justify={"space-between"} marginTop={"150px"}>
-                    <Image                       
-                        src={gradientImg}
-                        alt="Gradient Image"
-                        objectFit="cover"
-                        width={"60%"}
-                    />
-                    <Flex align={"flex-end"} direction={"column"} marginRight={"10%"}>
-                        <Text marginTop={"50px"} fontWeight={"bold"}>Web Developer Intern</Text>
-                        <Text marginTop={"10px"}>SoftWeb Adaptive I.T. Solutions</Text>
-                        <Text marginTop={"20px"} fontSize={"14px"} fontWeight={"bold"}>07/2023 - 09/2023</Text>
 
-                    </Flex>
-                </Flex>
-
-
+                <JobCard
+                    image={gradientImg}
+                    title="Web Developer Intern"
+                    company="SoftWeb Adaptive I.T. Solutions"
+                    duration="07/2023 - 09/2023"
+                    responsibilities={["Built React components","Integrated REST APIs","Daily standups & code reviews"]}
+                    tools={'Angular / Node.js / MongoDB / Postman'}
+                />
             </Container>
         </>
     )
