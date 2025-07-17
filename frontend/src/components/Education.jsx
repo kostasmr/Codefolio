b905ee01e965603352bd50e515de447f57f2ae89
Konstantinos Mourousidis <kwstasmr7@gmail.com>
2025-07-17 16:31:55 +0300

    Update Education.jsx

--- frontend/src/components/Education.jsx
+++ frontend/src/components/Education.jsx
@@ -1,29 +1,34 @@
 import gradientImg from '../assets/images/education-image.png'
-import { Container, Flex, Text, Image} from "@chakra-ui/react";
+import { Container, Text, Image, Grid, GridItem, Flex} from "@chakra-ui/react";
 
 function Education () {
     return (
         <>
-            <Container id="education" maxW="100%" height="auto" bg={"black"} paddingTop={"200px"}>
-                <Flex justify={"space-between"}>
-                    <Flex marginLeft={"10%"} direction={"column"}>
-                        <Text>/02</Text>
-                        <Text marginTop={"180px"} fontSize={"26px"}>Education</Text>
-                        <Text fontWeight={"bold"} marginTop={"280px"}>Integrated Master (5 years degree),<br/> Computer Science & Engineering</Text>
-                        <Text marginTop={"25px"}>University of Ioannina</Text>
-                        <Text fontWeight={"bold"} fontSize={"14px"} marginTop={"10px"}>9/2017 - 6/2023</Text>
-                    </Flex>
-                    
-                    <Image                       
-                        src={gradientImg}
-                        alt="Gradient Image"
-                        objectFit="cover"
-                        width="500px"
-                        height="100vh"
-                    />
-
-                </Flex>
-
+            <Container id="education"  h="100vh" maxW="90%" ms={"10%"}>
+                <Grid templateColumns="repeat(3, 1fr)" gap="1" h="full">
+                    <GridItem colSpan={2}>
+                        <Flex h={"full"} direction={"column"}>
+                            <Flex direction={"column"}>
+                                <Text>/02</Text>
+                                <Text fontSize={"26px"} mt={"100px"}>Education</Text>
+                            </Flex>
+                            <Flex h={"full"} direction={"column"} justify={"end"} mb="150px">
+                                <Text fontWeight={"bold"}>Integrated Master (5 years degree),<br/> Computer Science & Engineering</Text>
+                                <Text mt="20px">University of Ioannina</Text>
+                                <Text fontWeight={"bold"} fontSize={"14px"} mt="10px">9/2017 - 6/2023</Text>
+                            </Flex>
+                        </Flex>
+                    </GridItem>
+                    <GridItem colSpan={1}>
+                        <Image                       
+                            src={gradientImg}
+                            alt="Gradient Image"
+                            objectFit="cover"
+                            width="full"
+                            h="full"
+                        />
+                    </GridItem>
+                </Grid>
             </Container>
         </>
     )
