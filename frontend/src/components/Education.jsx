051499d9556a5c55b7d67587d6793aad792222bb
Konstantinos Mourousidis <kwstasmr7@gmail.com>
2025-07-24 17:20:11 +0300

    Update Education.jsx

--- frontend/src/components/Education.jsx
+++ frontend/src/components/Education.jsx
@@ -1,5 +1,10 @@
 import gradientImg from '../assets/images/education-image.png'
 import { Container, Text, Image, Grid, GridItem, Flex} from "@chakra-ui/react";
+import { motion } from "framer-motion";
+
+const MotionFlex = motion(Flex);
+const MotionImage = motion(Image);
+
 
 function Education () {
     return (
@@ -8,24 +13,48 @@ function Education () {
                 <Grid templateColumns="repeat(3, 1fr)" gap="1" h="full">
                     <GridItem colSpan={2}>
                         <Flex h={"full"} direction={"column"}>
-                            <Flex direction={"column"}>
+                            {/* Section heading */}
+                            <MotionFlex
+                                direction="column"
+                                initial={{ opacity: 0, x: -100 }}
+                                whileInView={{ opacity: 1, x: 0 }}
+                                transition={{ duration: 1 }}
+                                viewport={{ once: true }}
+                            >
                                 <Text>/02</Text>
-                                <Text fontSize={"26px"} mt={"100px"}>Education</Text>
-                            </Flex>
-                            <Flex h={"full"} direction={"column"} justify={"end"} mb="150px">
-                                <Text fontWeight={"bold"}>Integrated Master (5 years degree),<br/> Computer Science & Engineering</Text>
+                                <Text fontSize="26px" mt="100px">Education</Text>
+                            </MotionFlex>
+
+                            {/* Degree info */}
+                            <MotionFlex
+                                h="full"
+                                direction="column"
+                                justify="end"
+                                mb="150px"
+                                initial={{ opacity: 0, x: -100 }}
+                                whileInView={{ opacity: 1, x: 0 }}
+                                transition={{ duration: 1, delay: 0.4 }}
+                                viewport={{ once: true }}
+                            >
+                                <Text fontWeight="bold">
+                                    Integrated Master (5 years degree),<br /> Computer Science & Engineering
+                                </Text>
                                 <Text mt="20px">University of Ioannina</Text>
-                                <Text fontWeight={"bold"} fontSize={"14px"} mt="10px">9/2017 - 6/2023</Text>
-                            </Flex>
+                                <Text fontWeight="bold" fontSize="14px" mt="10px">9/2017 - 6/2023</Text>
+                            </MotionFlex>
                         </Flex>
                     </GridItem>
                     <GridItem colSpan={1}>
-                        <Image                       
+                        <MotionImage
                             src={gradientImg}
                             alt="Gradient Image"
                             objectFit="cover"
-                            width="full"
-                            h="full"
+                            width="100%"
+                            height="100%"
+                            initial={{ opacity: 0 }}
+                            whileInView={{ opacity: 1 }}
+                            transition={{ duration: 1.4 }}
+                            viewport={{ once: true }}
                         />
                     </GridItem>
                 </Grid>
