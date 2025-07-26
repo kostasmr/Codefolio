8003b0c054e948b89e2cbcf803554b1172e3eed9
Konstantinos Mourousidis <kwstasmr7@gmail.com>
2025-07-26 16:38:30 +0300

    Update Education.jsx

--- frontend/src/components/Education.jsx
+++ frontend/src/components/Education.jsx
@@ -9,7 +9,7 @@ const MotionImage = motion(Image);
 function Education () {
     return (
         <>
-            <Container id="education"  h="100vh" maxW="90%" ms={"10%"}>
+            <Container id="education"  h={{ sm: "40vh", tablet:"60vh", lg:"100vh"}} maxW={{sm: "95%",lg: "90%"}} ms={{sm: "5%", lg:"10%"}}>
                 <Grid templateColumns="repeat(3, 1fr)" gap="1" h="full">
                     <GridItem colSpan={2}>
                         <Flex h={"full"} direction={"column"}>
@@ -21,8 +21,8 @@ function Education () {
                                 transition={{ duration: 1 }}
                                 viewport={{ once: true }}
                             >
-                                <Text>/02</Text>
-                                <Text fontSize="26px" mt="100px">Education</Text>
+                                <Text textStyle="t">/02</Text>
+                                <Text textStyle="sh" mt={{sm: "50px",tablet: "100px"}}>Education</Text>
                             </MotionFlex>
 
                             {/* Degree info */}
@@ -30,17 +30,17 @@ function Education () {
                                 h="full"
                                 direction="column"
                                 justify="end"
-                                mb="150px"
+                                mb={{sm: "50px",tablet: "100px",lg: "150px"}}
                                 initial={{ opacity: 0, x: -100 }}
                                 whileInView={{ opacity: 1, x: 0 }}
                                 transition={{ duration: 1, delay: 0.4 }}
                                 viewport={{ once: true }}
                             >
-                                <Text fontWeight="bold">
+                                <Text textStyle="tb">
                                     Integrated Master (5 years degree),<br /> Computer Science & Engineering
                                 </Text>
-                                <Text mt="20px">University of Ioannina</Text>
-                                <Text fontWeight="bold" fontSize="14px" mt="10px">9/2017 - 6/2023</Text>
+                                <Text mt={{sm: "10px", tablet:"20px"}} textStyle="t">University of Ioannina</Text>
+                                <Text textStyle="tb" mt={{sm: "5px",tablet: "10px"}}>9/2017 - 6/2023</Text>
                             </MotionFlex>
                         </Flex>
                     </GridItem>
