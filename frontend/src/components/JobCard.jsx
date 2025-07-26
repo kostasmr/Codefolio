3a8d02ad9bacf06ebf37182ad2c4fc1b5183eaff
Konstantinos Mourousidis <kwstasmr7@gmail.com>
2025-07-26 16:38:45 +0300

    Update JobCard.jsx

--- frontend/src/components/JobCard.jsx
+++ frontend/src/components/JobCard.jsx
@@ -10,47 +10,47 @@ const JobCard = ({ image, title, company, duration, responsibilities, tools }) =
     const [showDetails, setShowDetails] = useState(false);
 
     return (
-        <Flex justify="space-between" h="100%"> 
-                <Box position="relative" width="60%" >
-                    <Image src={image} alt={`${title}`} width="100%" h="100%" objectFit="cover"/>
+        <Flex justify="space-between" h="100%" direction={{sm: "column", tablet:"row"}} align={{sm: "center", tablet: "start"}}> 
+            <Box position="relative" w={{sm: "90%",tablet: "60%"}}>
+                <Image src={image} alt={`${title}`} width="100%" h="100%" objectFit="cover"/>
 
-                    {showDetails && 
-                        <Box
-                            position="absolute"
-                            top={0}
-                            left={0}
-                            w="100%"
-                            h="100%"
-                            bg="rgba(0,0,0,0.6)"
-                            color="white"
-                            transition="0.3s ease"
-                        >
-                            <Grid templateColumns="repeat(10, 1fr)" gap="1"  h={"full"}>
-                                <GridItem colSpan={4} ms={"10%"} mt={"15%"}>
-                                        <Text fontWeight="bold" fontSize={"32px"}>{title}</Text>
-                                        <Text mt={"20px"} fontSize="14px">at {company}</Text>
-                                </GridItem>
-                                <GridItem colSpan={6}>
-                                    <Flex gap={"20px"} direction={"column"} h={"full"} justify={"center"} w={"90%"}>
-                                        {responsibilities.map((item, idx) => (
-                                            <Text key={idx}>• {item}</Text>
-                                        ))}
-                                        <Text mt={"20px"} fontSize="14px">{tools}</Text>
-                                    </Flex>
-                                </GridItem>
-                            </Grid>
-                        </Box>
-                    }
-                </Box>
+                {showDetails && 
+                    <Box
+                        position="absolute"
+                        top={0}
+                        left={0}
+                        w="100%"
+                        h="100%"
+                        bg="rgba(0,0,0,0.6)"
+                        color="white"
+                        transition="0.3s ease"
+                    >
+                        <Grid templateColumns="repeat(10, 1fr)" gap="1"  h={"full"}>
+                            <GridItem colSpan={4} ms={{sm: "5%",lg: "10%"}} mt={{sm: "10%",lg :"15%"}}>
+                                    <Text textStyle="sh">{title}</Text>
+                                    <Text mt={{sm: "10px",lg: "20px"}} textStyle="t">at {company}</Text>
+                            </GridItem>
+                            <GridItem colSpan={6}>
+                                <Flex gap={{sm: "5px",tablet: "10px",lg: "20px"}} direction={"column"} h={"full"} justify={"center"} w={{sm: "95%",lg: "90%"}}>
+                                    {responsibilities.map((item, idx) => (
+                                        <Text key={idx} textStyle="ts">• {item}</Text>
+                                    ))}
+                                    <Text mt={"20px"} textStyle="ts">{tools}</Text>
+                                </Flex>
+                            </GridItem>
+                        </Grid>
+                    </Box>
+                }
+            </Box>
 
             <Flex
                 direction="column"
-                mr="10%"
+                mr={{sm: "5%",lg:"10%"}}
                 color="white" 
             >
-                <Flex justify={"flex-end"} align={"center"} marginBottom={"10px"} mr="20px">
-                    <Text marginEnd={"20px"} letterSpacing={"5px"} color={"#D9D9D940"}>hover</Text>
-                    <TbHandClick color={"#D9D9D960"} />             
+                <Flex justify={"flex-end"} align={"center"} marginBottom={{sm: "5px",lg: "10px"}} mr="20px" display={{sm: "none", tablet: "flex"}}>
+                    <Text marginEnd={"20px"} letterSpacing={"5px"} color={"#D9D9D940"} textStyle="t">hover</Text>
+                    <TbHandClick color={"#D9D9D960"}/>             
                 </Flex>
                 
                 <Flex
@@ -59,13 +59,15 @@ const JobCard = ({ image, title, company, duration, responsibilities, tools }) =
                     h={"fit-content"}
                     onMouseEnter={() => setShowDetails(true)}
                     onMouseLeave={() => setShowDetails(false)}
+                    // onClick={handleClick}
                     _hover={{bg: "#D9D9D910"}}
-                    p={"20px"}
-                    textAlign={"end"}
+                    p={{sm: "10px",lg: "20px"}}
+                    textAlign={{sm: "center",tablet: "end"}}
+                    mt={{sm: "20px", tablet: "0"}}
                 >
-                    <Text fontWeight="bold">{title}</Text>
-                    <Text mt="10px">{company}</Text>
-                    <Text mt="20px" fontSize="14px" fontWeight="bold">{duration}</Text>
+                    <Text textStyle="tb">{title}</Text>
+                    <Text mt={{sm: "5px",lg: "10px"}} textStyle="t">{company}</Text>
+                    <Text mt={{sm: "10px",lg: "20px"}} textStyle="tb">{duration}</Text>
                 </Flex>
             </Flex>
         </Flex>
