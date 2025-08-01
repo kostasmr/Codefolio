39d5f492043f261cfc899c160f55fdf25b2f5e9d
Konstantinos Mourousidis <kwstasmr7@gmail.com>
2025-08-01 16:28:02 +0300

    Update Contact.jsx

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -1,8 +1,13 @@
 import gradientImg from '../assets/images/letswork-image.png';
 import { Container, Image, Text, Flex, Link, Box} from "@chakra-ui/react";
 import { FaLinkedin, FaGithubSquare } from "react-icons/fa";
+import { useLanguage } from '../tools/translation/useLanguage';
+
 
 function Contact () {
+
+    const {t} = useLanguage();
+
     return (
         <>
             <Container id="contact" maxW="100%" h={{ sm: "40vh", tablet:"60vh", desktop:"100vh", lg:"100vh"}} padding={{ sm: "1", tablet:"3", desktop:"5"}} bg={"black"} position={'relative'}>
@@ -31,7 +36,7 @@ function Contact () {
                         paddingInline={{sm: "10px", tablet: "25px",lg:"40px"}}
                         paddingTop={{sm: "10px",tablet: "15px",lg:"20px"}}
                     > 
-                        <Text textStyle="tb">Thessaloniki, Greece</Text>                
+                        <Text textStyle="tb">{t["thess"]}, {t["gr"]}</Text>                
                         <Text textStyle="t">+30 697 596 0090</Text>
                     </Flex>
 
@@ -45,7 +50,7 @@ function Contact () {
                             letterSpacing={{sm: "3px", tablet: "5px",lg: "10px"}}
                             align={"center"} 
                             >
-                                LETS WORK <br /> TOGETHER
+                                {t["LETSWORK"]} <br /> {t["TOGETHER"]}
                             </Text>
                     </Flex>
                     <Flex                    
@@ -54,16 +59,16 @@ function Contact () {
                         justifyContent={"space-between"}
                         align={"end"}
                     >
-                        <Text textStyle="tb">Send me a message</Text>
-                        <Text textStyle="t" marginRight={{sm: "38px",tablet: "30px",desktop: "90px"}}>kmourousidis@gmail.com</Text>
+                        <Text textStyle="tb">{t["sendme"]}</Text>
+                        <Text textStyle="t" marginRight={{sm: "38px",tablet: "30px",desktop: "120px"}}>kmourousidis@gmail.com</Text>
                         <Flex gap={{sm: 2,tablet: 5}}>
-                            <Link href="https://www.linkedin.com/in/konstantinos-mourousidis" isExternal _hover={{ color: "orange.400" }}>
-                                <Box fontSize={{ sm: "20px", tablet: "30px", desktop: "40px", lg: "50px" }}>
+                            <Link href="https://www.linkedin.com/in/konstantinos-mourousidis" isExternal transition="all 0.3s ease" _hover={{ color: "orange.400", transform: "translateY(-4px)" }}>
+                                <Box fontSize={{ sm: "20px", tablet: "30px", desktop: "40px", lg: "40px" }}>
                                     <FaLinkedin />
                                 </Box>
                             </Link>
-                            <Link href="https://github.com/kostasmr" isExternal _hover={{ color: "orange.400" }}>
-                                <Box fontSize={{ sm: "20px", tablet: "30px", desktop: "40px", lg: "50px" }}>
+                            <Link href="https://github.com/kostasmr" isExternal transition="all 0.3s ease" _hover={{ color: "orange.400", transform: "translateY(-4px)" }}>
+                                <Box fontSize={{ sm: "20px", tablet: "30px", desktop: "40px", lg: "40px" }}>
                                     <FaGithubSquare />
                                 </Box>
                             </Link>
